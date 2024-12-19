eb93ff2c9390f2dad3ca033389a453fe3eb7ebba
hamzahraihanif <hamzahraihanif@gmail.com>
2024-12-19 10:30:05 +0000

    change import name file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:expense_tracker/app.dart';
+import 'package:expense_tracker/view/screens/home_screen.dart';
 
 
 void main() {
