61bd0c737ae6bb07e50c510f3d7f7d9afcc95aaa
hamzahraihanif <hamzahraihanif@gmail.com>
2024-12-19 10:29:40 +0000

    add mvvm folder structure

---
+++ lib/view/screens/home_screen.dart
@@ -0,0 +1,88 @@
+import 'package:expense_tracker/view/widgets/income_expenses_card_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter MVVM',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.white, brightness: Brightness.light),
+        fontFamily: GoogleFonts.inter().fontFamily,
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Expenses'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    AppBar appBar = AppBar(
+      centerTitle: true,
+      title: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey, width: 0.5),
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+        child: Text(widget.title, style: const TextStyle(fontSize: 14.0)),
+      ),
+      leading: const Padding(
+          padding: EdgeInsets.only(left: 16.0), child: CircleAvatar()),
+      actions: [
+        Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: IconButton(
+                onPressed: () => {}, icon: const Icon(Icons.notifications)))
+      ],
+    );
+    return Scaffold(
+        appBar: appBar,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Center(
+                child: Text('Account Balances'),
+              ),
+              const Center(
+                child: Text(
+                  '\$9000',
+                  style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.w700),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.all(10),
+                height: 200,
+                child: GridView.count(
+                  crossAxisSpacing: 10,
+                  shrinkWrap: true,
+                  childAspectRatio: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisCount: 2,
+                  children: const [
+                    IncomeExpensesCardWidget(title: 'Income', amount: 2000),
+                    IncomeExpensesCardWidget(title: 'Expense', amount: 2000)
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
