d4102a2ca217fb920f3af0fff45c59330eec89f4
hamzahraihanif <hamzahraihanif@gmail.com>
2024-12-20 12:38:11 +0000

    change data type for amount

--- lib/view/widgets/income_expenses_card_widget.dart
+++ lib/view/widgets/income_expenses_card_widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class IncomeExpensesCardWidget extends StatefulWidget {
   final String title;
-  final double amount;
+  final int amount;
 
   const IncomeExpensesCardWidget(
       {super.key, required this.title, required this.amount});
@@ -13,34 +13,47 @@ class IncomeExpensesCardWidget extends StatefulWidget {
 
 class _IncomeExpensesCardWidget extends State<IncomeExpensesCardWidget> {
   String get title => widget.title;
-  double get amount => widget.amount;
+  int get amount => widget.amount;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 50,
+      width: double.infinity,
       child: Card(
           color: Colors.teal[100],
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(15.0),
           ),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(
-                height: 50,
-                width: 50,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  color: Colors.white,
+              Flexible(
+                flex: 1,
+                child: Container(
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.white,
+                  ),
+                  child: const Icon(Icons.monetization_on),
                 ),
-                child: const Icon(Icons.monetization_on),
               ),
-              Container(
-                padding: const EdgeInsets.all(20),
-                child: Column(
-                  children: [Text(title), Text(amount.toString())],
-                ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: const TextStyle(fontSize: 14.0),
+                  ),
+                  Text(
+                    '\$${amount.toString()}',
+                    style: const TextStyle(
+                        fontSize: 18.0, fontWeight: FontWeight.w800),
+                  )
+                ],
               ),
             ],
           )),
