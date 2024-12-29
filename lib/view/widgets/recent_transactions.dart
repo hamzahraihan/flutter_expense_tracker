3f4808557d25ad70d141cdfbad79a7575ef9785e
hamzahraihanif <hamzahraihanif@gmail.com>
2024-12-29 07:17:27 +0000

    change recent transaction widget styling

--- lib/view/widgets/recent_transactions.dart
+++ lib/view/widgets/recent_transactions.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class RecentTransactionsWidget extends StatefulWidget {
-  final Icon icon;
+  final IconData icon;
   final String title;
   final String description;
   const RecentTransactionsWidget(
@@ -15,12 +15,47 @@ class RecentTransactionsWidget extends StatefulWidget {
 }
 
 class _RecentTransactionState extends State<RecentTransactionsWidget> {
-  Icon get icon => widget.icon;
+  IconData get icon => widget.icon;
   String get title => widget.title;
   String get description => widget.description;
 
   @override
   Widget build(BuildContext context) {
-    return const Row(children: [Text('Test')]);
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15.0),
+        color: Colors.grey,
+      ),
+      padding: const EdgeInsets.all(16.0),
+      width: double.infinity,
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Flexible(
+            fit: FlexFit.loose,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: Colors.white,
+              ),
+              padding: const EdgeInsets.all(16.0),
+              child: Icon(icon),
+            ),
+          ),
+          Flexible(
+            fit: FlexFit.tight,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.0),
+                color: Colors.white,
+              ),
+              padding: const EdgeInsets.all(16.0),
+              child: Icon(icon),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
