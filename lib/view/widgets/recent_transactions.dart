0e1023ec7b511dd3e6384b54dda59c1bbaa4755b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-06 08:45:55 +0700

    add expense type, date, and amount to recent transactions widget

--- lib/view/widgets/recent_transactions.dart
+++ lib/view/widgets/recent_transactions.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/model/transactions_model.dart';
 import 'package:expense_tracker/view/util/expenses_color.dart';
 import 'package:flutter/material.dart';
 
@@ -5,11 +6,17 @@ class RecentTransactionsWidget extends StatefulWidget {
   final IconData icon;
   final String title;
   final String description;
+  final ExpenseType expenseType;
+  final DateTime date;
+  final double amount;
   const RecentTransactionsWidget(
       {super.key,
       required this.icon,
       required this.title,
-      required this.description});
+      required this.description,
+      required this.expenseType,
+      required this.date,
+      required this.amount});
 
   @override
   State<RecentTransactionsWidget> createState() => _RecentTransactionState();
@@ -19,6 +26,7 @@ class _RecentTransactionState extends State<RecentTransactionsWidget> {
   IconData get icon => widget.icon;
   String get title => widget.title;
   String get description => widget.description;
+  ExpenseType get expenseType => widget.expenseType;
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +47,8 @@ class _RecentTransactionState extends State<RecentTransactionsWidget> {
               borderRadius: BorderRadius.circular(15.0),
               color: ExpensesColor(expenseTitle: title).getColorBackground(),
             ),
-            padding: const EdgeInsets.all(16.0),
+            height: double.infinity,
+            width: 70,
             child: Icon(
               icon,
               color: ExpensesColor(expenseTitle: title).getColorIcon(),
@@ -49,24 +58,37 @@ class _RecentTransactionState extends State<RecentTransactionsWidget> {
             width: 10,
           ),
           Expanded(
+            flex: 4,
             child: Container(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(15.0),
               ),
-              padding: const EdgeInsets.all(2.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Text(title, textAlign: TextAlign.start),
                   Text(description,
                       overflow: TextOverflow.ellipsis,
                       softWrap: true,
-                      textAlign: TextAlign.start)
+                      textAlign: TextAlign.start,
+                      style:
+                          const TextStyle(color: Colors.black38, fontSize: 12))
                 ],
               ),
             ),
-          )
+          ),
+          const Expanded(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '2000',
+                  ),
+                  Text('2000')
+                ],
+              ))
         ],
       ),
     );
