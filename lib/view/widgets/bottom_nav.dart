99f1537dbfea1162f865a3352998e7446f474961
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-06 19:17:15 +0700

    implement BottomNavigationBarWidget with navigation buttons and state management

--- lib/view/widgets/bottom_nav.dart
+++ lib/view/widgets/bottom_nav.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 
-class BottomNavigationBarWidget extends StatefulWidget{
+class BottomNavigationBarWidget extends StatefulWidget {
   const BottomNavigationBarWidget({super.key});
-  
+
   @override
-  State<BottomNavigationBarWidget> createState() => _BottomNavigationState() ;
+  State<BottomNavigationBarWidget> createState() => _BottomNavigationState();
 }
 
-class _BottomNavigationState extends State<BottomNavigationBarWidget>{
+class NavButton {
+  final IconData icon;
+  final String label;
+
+  NavButton({required this.icon, required this.label});
+}
+
+class _BottomNavigationState extends State<BottomNavigationBarWidget> {
+  int _selectedIndex = 0;
+  final List<NavButton> _navButton = [
+    NavButton(icon: Icons.home, label: 'Home'),
+    NavButton(icon: Icons.payments, label: 'Transactions'),
+    NavButton(icon: Icons.pie_chart, label: 'Budget')
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(items: [],);
+    return BottomNavigationBar(
+      items: _navButton.map((item) {
+        return BottomNavigationBarItem(
+            icon: Icon(item.icon), label: item.label);
+      }).toList(),
+      currentIndex: _selectedIndex,
+      selectedItemColor: Colors.blueAccent.shade200,
+      onTap: _onItemTapped,
+    );
   }
 }
