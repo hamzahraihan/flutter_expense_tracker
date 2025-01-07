616183a694cd331c590123f0eb69ed0fdcb87754
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-07 19:15:28 +0700

    add ExpenseScreen widget with basic layout and routing setup

--- lib/view/screens/expense_screen.dart
+++ lib/view/screens/expense_screen.dart
@@ -1,1 +1,13 @@
-class ExpenseScreen {}
+import 'package:flutter/material.dart';
+
+class ExpenseScreen extends StatelessWidget {
+  const ExpenseScreen({super.key});
+  static const String routeName = '/expense';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Expense Screen'),
+    );
+  }
+}
