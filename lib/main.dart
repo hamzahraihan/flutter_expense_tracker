86225040b34b9102ad368ef35b6bcd8abb4349c7
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-07 16:01:20 +0700

    add MyApp widget with MaterialApp configuration and Google Fonts integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/view/screens/home_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Expense',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.white, brightness: Brightness.light),
+        fontFamily: GoogleFonts.inter().fontFamily,
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyHomePage(title: 'Expenses'),
+        // '/expenses': (context) {}
+      },
+    );
+  }
+}
