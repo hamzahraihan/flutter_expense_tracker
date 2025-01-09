835b5fd4e481c14af3b55daa7dee51e139b726b6
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-09 20:41:10 +0700

    feat: initialize Firebase and load environment variables in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'package:expense_tracker/firebase_options.dart';
 import 'package:expense_tracker/view/screens/app.dart';
 import 'package:expense_tracker/view/screens/transactions_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_strategy/url_strategy.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env.local');
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform);
+
   setPathUrlStrategy();
   runApp(const MyApp());
 }
