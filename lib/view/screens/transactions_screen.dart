eff690c579e55de13b3e2aea1b3b43a9a9c5a1e1
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-09 15:46:41 +0700

    refactor: improve layout and readability of TransactionsScreen

--- lib/view/screens/transactions_screen.dart
+++ lib/view/screens/transactions_screen.dart
@@ -9,124 +9,143 @@ class TransactionsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final List<TransactionsModel> todayTransactions =
-        Transactions.filterTransactionsByDate(transactionsDataList).today;
+        Transactions.filterTransactionsByDate(transactionsDataList)
+            .today;
     final List<TransactionsModel> yesterdayTransactions =
-        Transactions.filterTransactionsByDate(transactionsDataList).yesterady;
+        Transactions.filterTransactionsByDate(transactionsDataList)
+            .yesterady;
     final List<TransactionsModel> thisWeekTransactions =
-        Transactions.filterTransactionsByDate(transactionsDataList).thisWeek;
+        Transactions.filterTransactionsByDate(transactionsDataList)
+            .thisWeek;
     final List<TransactionsModel> thisMonthTransaction =
-        Transactions.filterTransactionsByDate(transactionsDataList).thisMonth;
+        Transactions.filterTransactionsByDate(transactionsDataList)
+            .thisMonth;
     final List<TransactionsModel> olderTransactions =
-        Transactions.filterTransactionsByDate(transactionsDataList).older;
+        Transactions.filterTransactionsByDate(transactionsDataList)
+            .older;
 
     return Scaffold(
+        appBar: AppBar(
+          forceMaterialTransparency: true,
+          centerTitle: true,
+          title: const Text(
+            'Transactions',
+            style: TextStyle(
+                fontSize: 17.0, fontWeight: FontWeight.bold),
+          ),
+        ),
         body: ListView(
-      padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      children: [
-        Padding(
-          padding: todayTransactions.isNotEmpty
-              ? const EdgeInsets.symmetric(vertical: 8.0)
-              : const EdgeInsets.all(0),
-          child: Text(
-            todayTransactions.isNotEmpty ? 'Today' : '',
-            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Column(
-            children: todayTransactions.map((item) {
-          return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: RecentTransactionsWidget(
-                  icon: Icons.shopping_bag,
-                  title: item.title,
-                  description: item.description,
-                  expenseType: item.expenseType,
-                  date: item.date,
-                  amount: item.amount));
-        }).toList()),
-        Padding(
-          padding: yesterdayTransactions.isNotEmpty
-              ? const EdgeInsets.symmetric(vertical: 8.0)
-              : const EdgeInsets.all(0),
-          child: Text(
-            yesterdayTransactions.isNotEmpty ? 'Yesterday' : '',
-            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Column(
-            children: yesterdayTransactions.map((item) {
-          return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: RecentTransactionsWidget(
-                  icon: Icons.shopping_bag,
-                  title: item.title,
-                  description: item.description,
-                  expenseType: item.expenseType,
-                  date: item.date,
-                  amount: item.amount));
-        }).toList()),
-        Padding(
-          padding: thisWeekTransactions.isNotEmpty
-              ? const EdgeInsets.symmetric(vertical: 8.0)
-              : const EdgeInsets.all(0),
-          child: Text(
-            thisWeekTransactions.isNotEmpty ? 'This Week' : '',
-            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Column(
-            children: thisWeekTransactions.map((item) {
-          return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: RecentTransactionsWidget(
-                  icon: Icons.shopping_bag,
-                  title: item.title,
-                  description: item.description,
-                  expenseType: item.expenseType,
-                  date: item.date,
-                  amount: item.amount));
-        }).toList()),
-        Padding(
-          padding: thisMonthTransaction.isNotEmpty
-              ? const EdgeInsets.symmetric(vertical: 8.0)
-              : const EdgeInsets.all(0),
-          child: Text(
-            thisMonthTransaction.isNotEmpty ? 'This Month' : '',
-            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Column(
-            children: thisMonthTransaction.map((item) {
-          return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: RecentTransactionsWidget(
-                  icon: Icons.shopping_bag,
-                  title: item.title,
-                  description: item.description,
-                  expenseType: item.expenseType,
-                  date: item.date,
-                  amount: item.amount));
-        }).toList()),
-        const Padding(
-          padding: EdgeInsets.symmetric(vertical: 8.0),
-          child: Text(
-            'Older',
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
-        ),
-        Column(
-            children: olderTransactions.map((item) {
-          return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 4.0),
-              child: RecentTransactionsWidget(
-                  icon: Icons.shopping_bag,
-                  title: item.title,
-                  description: item.description,
-                  expenseType: item.expenseType,
-                  date: item.date,
-                  amount: item.amount));
-        }).toList()),
-      ],
-    ));
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          children: [
+            Padding(
+              padding: todayTransactions.isNotEmpty
+                  ? const EdgeInsets.symmetric(vertical: 8.0)
+                  : const EdgeInsets.all(0),
+              child: Text(
+                todayTransactions.isNotEmpty ? 'Today' : '',
+                style: const TextStyle(
+                    fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Column(
+                children: todayTransactions.map((item) {
+              return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: RecentTransactionsWidget(
+                      icon: Icons.shopping_bag,
+                      title: item.title,
+                      description: item.description,
+                      expenseType: item.expenseType,
+                      date: item.date,
+                      amount: item.amount));
+            }).toList()),
+            Padding(
+              padding: yesterdayTransactions.isNotEmpty
+                  ? const EdgeInsets.symmetric(vertical: 8.0)
+                  : const EdgeInsets.all(0),
+              child: Text(
+                yesterdayTransactions.isNotEmpty ? 'Yesterday' : '',
+                style: const TextStyle(
+                    fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Column(
+                children: yesterdayTransactions.map((item) {
+              return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: RecentTransactionsWidget(
+                      icon: Icons.shopping_bag,
+                      title: item.title,
+                      description: item.description,
+                      expenseType: item.expenseType,
+                      date: item.date,
+                      amount: item.amount));
+            }).toList()),
+            Padding(
+              padding: thisWeekTransactions.isNotEmpty
+                  ? const EdgeInsets.symmetric(vertical: 8.0)
+                  : const EdgeInsets.all(0),
+              child: Text(
+                thisWeekTransactions.isNotEmpty ? 'This Week' : '',
+                style: const TextStyle(
+                    fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Column(
+                children: thisWeekTransactions.map((item) {
+              return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: RecentTransactionsWidget(
+                      icon: Icons.shopping_bag,
+                      title: item.title,
+                      description: item.description,
+                      expenseType: item.expenseType,
+                      date: item.date,
+                      amount: item.amount));
+            }).toList()),
+            Padding(
+              padding: thisMonthTransaction.isNotEmpty
+                  ? const EdgeInsets.symmetric(vertical: 8.0)
+                  : const EdgeInsets.all(0),
+              child: Text(
+                thisMonthTransaction.isNotEmpty ? 'This Month' : '',
+                style: const TextStyle(
+                    fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Column(
+                children: thisMonthTransaction.map((item) {
+              return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: RecentTransactionsWidget(
+                      icon: Icons.shopping_bag,
+                      title: item.title,
+                      description: item.description,
+                      expenseType: item.expenseType,
+                      date: item.date,
+                      amount: item.amount));
+            }).toList()),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
+                'Older',
+                style: TextStyle(
+                    fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            ),
+            Column(
+                children: olderTransactions.map((item) {
+              return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: RecentTransactionsWidget(
+                      icon: Icons.shopping_bag,
+                      title: item.title,
+                      description: item.description,
+                      expenseType: item.expenseType,
+                      date: item.date,
+                      amount: item.amount));
+            }).toList()),
+          ],
+        ));
   }
 }
