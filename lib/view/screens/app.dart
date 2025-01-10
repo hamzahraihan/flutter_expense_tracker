971ba2094b555396a08d6648a07e0152844963f6
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-10 14:46:06 +0700

    fix: update import paths for home and transactions screens

--- lib/view/screens/app.dart
+++ lib/view/screens/app.dart
@@ -1,5 +1,5 @@
-import 'package:expense_tracker/view/screens/home_screen.dart';
-import 'package:expense_tracker/view/screens/transactions_screen.dart';
+import 'package:expense_tracker/view/screens/home/home_screen.dart';
+import 'package:expense_tracker/view/screens/transactions/transactions_screen.dart';
 
 import 'package:flutter/material.dart';
 
