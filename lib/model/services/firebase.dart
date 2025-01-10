974cd134b3cffbf090c4da582ea0061d05f0e0c0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-10 12:47:18 +0700

    feat: implement Firestore data retrieval in Firebase services model

--- lib/model/services/firebase.dart
+++ lib/model/services/firebase.dart
@@ -1,1 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 // TODO create a firebase services model
+
+FirebaseFirestore db = FirebaseFirestore.instance;
+
+Future<DocumentSnapshot> retrieveData(String collectionName) async {
+  CollectionReference data =
+      FirebaseFirestore.instance.collection(collectionName);
+  return data.doc().get();
+}
