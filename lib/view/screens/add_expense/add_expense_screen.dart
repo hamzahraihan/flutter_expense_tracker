f2b682aceb34fa5dec76c50e9629a7f8632f2902
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-12 16:28:22 +0700

    feat: enhance AddExpenseScreen UI with improved app bar and input fields

--- lib/view/screens/add_expense/add_expense_screen.dart
+++ lib/view/screens/add_expense/add_expense_screen.dart
@@ -12,10 +12,71 @@ class _AddExpenseScreenState extends State<AddExpenseScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: Text('Add expense'),
-      ),
-    );
+        appBar: AppBar(
+          centerTitle: true,
+          foregroundColor: Colors.white,
+          title: const Text(
+            'Expense',
+            style: TextStyle(fontSize: 18.0),
+          ),
+          backgroundColor: Colors.red.shade400,
+        ),
+        backgroundColor: Colors.red.shade400,
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 100,
+              ),
+              const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    'How much?',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white70,
+                    ),
+                  )),
+              Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Row(
+                    children: [
+                      const Flexible(
+                        child: Text(
+                          'Rp.',
+                          style: TextStyle(
+                              color: Colors.white, fontSize: 32.0),
+                        ),
+                      ),
+                      Flexible(
+                        child: TextField(
+                          keyboardType: TextInputType.number,
+                          style: const TextStyle(
+                              color: Colors.white, fontSize: 32.0),
+                          decoration: InputDecoration(
+                              hintStyle: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 32.0),
+                              hintText: '0',
+                              border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.circular(10),
+                                  borderSide: BorderSide.none)),
+                        ),
+                      ),
+                    ],
+                  )),
+              Container(
+                height: 500,
+                width: double.infinity,
+                decoration: const BoxDecoration(color: Colors.white),
+                child: const Text('data'),
+              ),
+            ],
+          ),
+        ));
   }
 }
