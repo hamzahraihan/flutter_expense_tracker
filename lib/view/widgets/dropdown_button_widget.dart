cd5a3cfa742f6fda12a49466a6337747b7abfb3f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-13 17:09:41 +0700

    feat: add DropdownButtonWidget for selection of categories

---
+++ lib/view/widgets/dropdown_button_widget.dart
@@ -0,0 +1,51 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+
+const List<String> list = <String>[
+  'Subscription',
+  'Food',
+  'Transportation',
+  'Shopping'
+];
+
+class DropdownButtonWidget extends StatefulWidget {
+  const DropdownButtonWidget({super.key});
+
+  @override
+  State<DropdownButtonWidget> createState() =>
+      _DropdownButtonWidgetState();
+}
+
+typedef MenuEntry = DropdownMenuEntry<String>;
+
+class _DropdownButtonWidgetState extends State<DropdownButtonWidget> {
+  static final List<MenuEntry> menuEntries =
+      UnmodifiableListView<MenuEntry>(list.map<MenuEntry>(
+          (String name) => MenuEntry(value: name, label: name)));
+
+  String dropdownValue = list.first;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Dropdown Button Component
+    return DropdownMenu(
+      menuStyle: const MenuStyle(
+        fixedSize: WidgetStatePropertyAll<Size>(Size.fromWidth(30)),
+      ),
+      inputDecorationTheme: const InputDecorationTheme(
+        border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black26),
+            borderRadius: BorderRadius.all(Radius.circular(16))),
+      ),
+      width: double.infinity,
+      initialSelection: dropdownValue,
+      dropdownMenuEntries: menuEntries,
+      onSelected: (value) {
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+    );
+  }
+}
