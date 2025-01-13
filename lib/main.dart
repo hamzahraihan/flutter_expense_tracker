0e651077910153891344549efc1bc1fadcd167f9
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-13 13:50:11 +0700

    fix: reorder dotenv loading and Firebase initialization in main function

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:url_strategy/url_strategy.dart';
 
 void main() async {
-  await dotenv.load(fileName: '.env.local');
   WidgetsFlutterBinding.ensureInitialized();
-  setPathUrlStrategy();
+
+  await dotenv.load(fileName: '.env.local');
   await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform);
 
