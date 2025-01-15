a03b6a3284e739b43b8b5a5935ee712e44ff71c0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-15 19:55:48 +0700

    feat: add TransactionsList widget for displaying a list of recent transactions

---
+++ lib/features/expense/screens/transactions/transactions_list.dart
@@ -0,0 +1,52 @@
+import 'package:expense_tracker/features/expense/widgets/recent_transactions.dart';
+import 'package:expense_tracker/model/transactions_model.dart';
+import 'package:flutter/material.dart';
+
+class TransactionsList extends StatefulWidget {
+  final List<TransactionsModel> transactionData;
+  final String dateTitle;
+  const TransactionsList(
+      {super.key,
+      required this.transactionData,
+      required this.dateTitle});
+  @override
+  State<TransactionsList> createState() => _TransactionsListState();
+}
+
+class _TransactionsListState extends State<TransactionsList> {
+  String get title => widget.dateTitle;
+  List<TransactionsModel> get transactions => widget.transactionData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: transactions.isNotEmpty
+            ? [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Text(
+                    title,
+                    style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Column(
+                    children: transactions.map((item) {
+                  return Padding(
+                      padding:
+                          const EdgeInsets.symmetric(vertical: 4.0),
+                      child: RecentTransactionsWidget(
+                        title: item.title,
+                        description: item.description,
+                        expenseType: item.expenseType,
+                        date: item.date,
+                        amount: item.amount,
+                        category: item.category,
+                      ));
+                }).toList()),
+              ]
+            : []);
+  }
+}
