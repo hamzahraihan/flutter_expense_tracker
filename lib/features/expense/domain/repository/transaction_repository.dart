6ed650eded81707bff4dfe9d3c4064d3750e9c7e
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-17 09:45:14 +0700

    feat: update TransactionRepository to include methods for saving, deleting, and adding transactions

--- lib/features/expense/domain/repository/transaction_repository.dart
+++ lib/features/expense/domain/repository/transaction_repository.dart
@@ -3,7 +3,10 @@ import 'package:expense_tracker/features/expense/data/model/transactions_model.d
 abstract class TransactionRepository {
   Future<List<TransactionsModel>> getTransactions();
 
-  Future<void> savedTransaction();
+  Future<void> savedTransaction(String id);
 
-  Future<void> deleteTransaction();
+  Future<void> deleteTransaction(String id);
+
+  Future<void> addExpenseTransaction(TransactionsModel transaction);
+  Future<void> addIncomeTransaction(TransactionsModel transaction);
 }
