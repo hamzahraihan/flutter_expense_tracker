ffa877fb84023f1e21aefbd1fdd57b57798141a8
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-17 09:03:33 +0700

    refactor: rename transactionsDataList method to retrieveTransactionData for clarity

--- lib/services/firebase.dart
+++ lib/services/firebase.dart
@@ -3,7 +3,7 @@ import 'package:expense_tracker/model/transactions_model.dart';
 
 FirebaseFirestore db = FirebaseFirestore.instance;
 
-Future<List<TransactionsModel>> transactionsDataList() async {
+Future<List<TransactionsModel>> retrieveTransactionData() async {
   final snapshot = await db.collection('transactions').get();
   return snapshot.docs
       .map((doc) => TransactionsModel.fromFirestore(doc, null))
