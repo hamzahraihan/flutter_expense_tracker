12eff37ae9066852b637771582be8e9043af169d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-18 19:00:48 +0700

    feat: integrate TransactionFirebaseBloc for managing transactions in MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:expense_tracker/features/expense/app.dart';
+import 'package:expense_tracker/features/expense/data/data_source/transactions_api_service.dart';
+import 'package:expense_tracker/features/expense/data/repository/transaction_repository_impl.dart';
+import 'package:expense_tracker/features/expense/domain/usecase/get_transactions.dart';
+import 'package:expense_tracker/features/expense/presentation/bloc/transaction/firebase/transaction_firebase_bloc.dart';
 import 'package:expense_tracker/features/expense/presentation/screens/transactions/transactions_screen.dart';
 import 'package:expense_tracker/features/expense/presentation/screens/upload/add_expense_screen.dart';
 import 'package:expense_tracker/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -21,39 +26,51 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My Expense',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.white, brightness: Brightness.light),
-        fontFamily: GoogleFonts.inter().fontFamily,
-        useMaterial3: true,
-      ),
-      initialRoute: '/',
-      home: const ExpenseTrackerApp(initialIndex: 0),
-      onGenerateRoute: (RouteSettings routeSettings) {
-        int initialIndex = 0;
-        switch (routeSettings.name) {
-          case TransactionsScreen.routeName:
-            initialIndex = 1;
-            break;
-          case '/budget':
-            initialIndex = 2;
-            break;
-          case AddExpenseScreen.routeName:
-            initialIndex = 3;
-            break;
-          default:
-            initialIndex = 0;
-            break;
-        }
-        return MaterialPageRoute(
-            builder: (BuildContext context) =>
-                ExpenseTrackerApp(initialIndex: initialIndex),
-            settings: routeSettings);
-      },
-    );
+    final TransactionsApiService transactionsApiService =
+        TransactionsApiService();
+    final TransactionRepositoryImpl transactionRepository =
+        TransactionRepositoryImpl(transactionsApiService);
+    final GetTransactionsUseCase getTransactionsUseCase =
+        GetTransactionsUseCase(transactionRepository);
+
+    return BlocProvider<TransactionFirebaseBloc>(
+        create: (context) =>
+            TransactionFirebaseBloc(getTransactionsUseCase),
+        child: MaterialApp(
+          title: 'My Expense',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.white,
+                brightness: Brightness.light),
+            fontFamily: GoogleFonts.inter().fontFamily,
+            useMaterial3: true,
+          ),
+          initialRoute: '/',
+          home: const ExpenseTrackerApp(initialIndex: 0),
+          onGenerateRoute: (RouteSettings routeSettings) {
+            int initialIndex = 0;
+            switch (routeSettings.name) {
+              case TransactionsScreen.routeName:
+                initialIndex = 1;
+                break;
+              case '/budget':
+                initialIndex = 2;
+                break;
+              case AddExpenseScreen.routeName:
+                initialIndex = 3;
+                break;
+              default:
+                initialIndex = 0;
+                break;
+            }
+            return MaterialPageRoute(
+                builder: (BuildContext context) =>
+                    ExpenseTrackerApp(initialIndex: initialIndex),
+                settings: routeSettings);
+          },
+        ));
   }
 }
