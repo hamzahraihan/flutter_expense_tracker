d295cd5649d7857ae2d0ad2b543c94f2dc884f06
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-18 19:00:59 +0700

    feat: implement TransactionFirebaseBloc to handle fetching transactions

--- lib/features/expense/presentation/bloc/transaction/firebase/transaction_firebase_bloc.dart
+++ lib/features/expense/presentation/bloc/transaction/firebase/transaction_firebase_bloc.dart
@@ -1,3 +1,31 @@
-class TransactionFirebaseBloc {
-  
+import 'package:bloc/bloc.dart';
+import 'package:expense_tracker/features/expense/domain/usecase/get_transactions.dart';
+import 'package:expense_tracker/features/expense/presentation/bloc/transaction/firebase/transaction_firebase_event.dart';
+import 'package:expense_tracker/features/expense/presentation/bloc/transaction/firebase/transaction_firebase_state.dart';
+
+class TransactionFirebaseBloc
+    extends Bloc<TransactionFirebaseEvent, TransactionFirebaseState> {
+  final GetTransactionsUseCase _getTransactionsUseCase;
+
+  TransactionFirebaseBloc(this._getTransactionsUseCase)
+      : super(const TransactionFirebaseState()) {
+    on<GetTransaction>(onfetchTransactions);
+  }
+
+  void onfetchTransactions(GetTransaction event,
+      Emitter<TransactionFirebaseState> emit) async {
+    final dataState = await _getTransactionsUseCase.execute();
+
+    emit(state.copyWith(status: TransactionStatus.loading));
+    try {
+      if (dataState.isNotEmpty) {
+        emit(state.copyWith(
+            status: TransactionStatus.success,
+            transactions: dataState));
+      }
+    } catch (e) {
+      throw Exception(
+          state.copyWith(status: TransactionStatus.failure));
+    }
+  }
 }
