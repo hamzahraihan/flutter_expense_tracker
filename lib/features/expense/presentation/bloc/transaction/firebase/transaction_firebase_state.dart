238df6ab6341cad9866b244874b37bedf93cf38b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-18 19:01:08 +0700

    feat: enhance TransactionFirebaseState with status management and equatable support

--- lib/features/expense/presentation/bloc/transaction/firebase/transaction_firebase_state.dart
+++ lib/features/expense/presentation/bloc/transaction/firebase/transaction_firebase_state.dart
@@ -1,1 +1,33 @@
-class TransactionFirebaseState {}
+import 'package:equatable/equatable.dart';
+import 'package:expense_tracker/features/expense/data/model/transactions_model.dart';
+
+enum TransactionStatus { initial, loading, success, failure }
+
+extension TransactionStatusX on TransactionStatus {
+  bool get isInitial => this == TransactionStatus.initial;
+  bool get isLoading => this == TransactionStatus.loading;
+  bool get isSuccess => this == TransactionStatus.success;
+  bool get isFailure => this == TransactionStatus.failure;
+}
+
+class TransactionFirebaseState extends Equatable {
+  final TransactionStatus status;
+  final List<TransactionsModel> transactions;
+
+  const TransactionFirebaseState(
+      {this.status = TransactionStatus.initial,
+      List<TransactionsModel>? transactions})
+      : transactions = transactions ?? const [];
+
+  TransactionFirebaseState copyWith({
+    TransactionStatus? status,
+    List<TransactionsModel>? transactions,
+  }) {
+    return TransactionFirebaseState(
+        status: status ?? this.status,
+        transactions: transactions ?? this.transactions);
+  }
+
+  @override
+  List<Object?> get props => [status, transactions];
+}
