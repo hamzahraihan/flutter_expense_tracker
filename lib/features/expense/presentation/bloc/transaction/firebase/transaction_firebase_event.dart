11e8161f70ee8cfd560211b02cd1fc50264d02ac
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-19 14:30:24 +0700

    feat: extend TransactionFirebaseEvent to include AddExpenseTransaction with transaction data

--- lib/features/expense/presentation/bloc/transaction/firebase/transaction_firebase_event.dart
+++ lib/features/expense/presentation/bloc/transaction/firebase/transaction_firebase_event.dart
@@ -1,7 +1,21 @@
-sealed class TransactionFirebaseEvent {
+import 'package:equatable/equatable.dart';
+import 'package:expense_tracker/features/expense/data/model/transactions_model.dart';
+
+sealed class TransactionFirebaseEvent extends Equatable {
   const TransactionFirebaseEvent();
+
+  @override
+  List<Object?> get props => [];
 }
 
 final class GetTransaction extends TransactionFirebaseEvent {
   const GetTransaction();
+}
+
+final class AddExpenseTransaction extends TransactionFirebaseEvent {
+  final Map<String, dynamic> transaction;
+  const AddExpenseTransaction(this.transaction);
+
+  @override
+  List<Object?> get props => [transaction];
 }
