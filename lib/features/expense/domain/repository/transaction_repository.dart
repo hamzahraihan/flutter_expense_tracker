305efbd66329e82af9b01a765e6ecc2fe0bfef61
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-21 18:04:34 +0700

    feat: remove unused filteredThisWeekExpenses method from TransactionRepository

--- lib/features/expense/domain/repository/transaction_repository.dart
+++ lib/features/expense/domain/repository/transaction_repository.dart
@@ -1,16 +1,14 @@
 import 'package:expense_tracker/features/expense/data/model/transactions_model.dart';
-import 'package:expense_tracker/features/expense/domain/entitiy/transaction_entities.dart';
 
 abstract class TransactionRepository {
   Future<List<TransactionsModel>> getTransactions();
-  Iterable<TransactionsModel> filteredThisWeekExpenses(
-      ExpenseType expenseType,
-      List<TransactionsModel> thisWeekTransactions);
+
   // Future<void> savedTransaction(String id);
 
   Future<void> deleteTransaction(String id);
 
   Future<void> addExpenseTransaction(
       Map<String, dynamic> transaction);
+
   Future<void> addIncomeTransaction(transaction);
 }
