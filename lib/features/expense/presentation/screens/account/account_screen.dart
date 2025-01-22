aba55bf3e548b0c6ce42b30c7ed160b97328025c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-22 14:33:32 +0700

    feat: add Account screen with basic layout and routing

---
+++ lib/features/expense/presentation/screens/account/account_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AccountScreen extends StatefulWidget {
+  const AccountScreen({super.key});
+  static const String routeName = 'Account';
+
+  @override
+  State<AccountScreen> createState() => _AccountScreenState();
+}
+
+class _AccountScreenState extends State<AccountScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        forceMaterialTransparency: false,
+        title: const Text('Account'),
+      ),
+      body: const Center(child: Text('Account')),
+    );
+  }
+}
