74e116f8ea7b9b088ffd06bbbd84084f7e5eb5d0
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-22 11:35:00 +0700

    feat: add expenseType to AddTransactionState and update copyWith method

--- lib/features/expense/presentation/bloc/add_transaction/add_transaction_state.dart
+++ lib/features/expense/presentation/bloc/add_transaction/add_transaction_state.dart
@@ -14,28 +14,36 @@ class AddTransactionState extends Equatable {
   final String descriptionValue;
   final String categoryValue;
   final int amountValue;
+  final String expenseType;
 
   const AddTransactionState(
       {this.status = AddTransactionStatus.initial,
       this.descriptionValue = '',
       this.amountValue = 0,
-      this.categoryValue = ''});
+      this.categoryValue = '',
+      this.expenseType = ''});
 
   AddTransactionState copyWith(
       {AddTransactionStatus? status,
       String? titleValue,
       String? descriptionValue,
       String? categoryValue,
-      int? amountValue}) {
+      int? amountValue,
+      String? expenseType}) {
     return AddTransactionState(
-      status: status ?? this.status,
-      descriptionValue: descriptionValue ?? this.descriptionValue,
-      categoryValue: categoryValue ?? this.categoryValue,
-      amountValue: amountValue ?? this.amountValue,
-    );
+        status: status ?? this.status,
+        descriptionValue: descriptionValue ?? this.descriptionValue,
+        categoryValue: categoryValue ?? this.categoryValue,
+        amountValue: amountValue ?? this.amountValue,
+        expenseType: expenseType ?? this.expenseType);
   }
 
   @override
-  List<Object?> get props =>
-      [status, descriptionValue, categoryValue, amountValue];
+  List<Object?> get props => [
+        status,
+        descriptionValue,
+        categoryValue,
+        amountValue,
+        expenseType
+      ];
 }
