50339d1e3aaedba043fac184573528f735ac6899
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-24 14:59:16 +0700

    feat: enhance AccountModel to support Firestore integration for account data

--- lib/features/expense/data/model/account_model.dart
+++ lib/features/expense/data/model/account_model.dart
@@ -1,1 +1,26 @@
-class AccountModel {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:expense_tracker/features/expense/domain/entitiy/account_entities.dart';
+
+class AccountModel extends AccountEntities {
+  const AccountModel(
+      {required super.balance, required super.walletType});
+
+  factory AccountModel.fromFirestore(
+    DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options,
+  ) {
+    final Map<String, dynamic>? data = snapshot.data();
+    return AccountModel(
+      balance: data?['balance'],
+      walletType:
+          WalletTypeExtension.fromString(data?['expenseType']),
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      "balance": balance,
+      "walletType": WalletTypeExtension(walletType).toShortString(),
+    };
+  }
+}
