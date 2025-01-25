897b44e340b1fd6f16fd3baba8662810eb67e63e
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-25 19:34:02 +0700

    feat: add FormStatus, EmailStatus, and PasswordStatus enums for form validation states

---
+++ lib/features/auth/presentation/bloc/status.dart
@@ -0,0 +1,12 @@
+enum FormStatus {
+  initial,
+  valid,
+  invalid,
+  submissionInProgress,
+  submissionSuccess,
+  submissionFailure,
+}
+
+enum EmailStatus { unknown, valid, invalid }
+
+enum PasswordStatus { unknown, valid, invalid }
