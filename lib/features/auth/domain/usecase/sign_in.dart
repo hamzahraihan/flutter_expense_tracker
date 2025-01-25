74653780efbaf494c60e1067db39c0f315da6d95
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-25 19:33:28 +0700

    feat: implement SignInUseCase for user sign-in functionality

--- lib/features/auth/domain/usecase/sign_in.dart
+++ lib/features/auth/domain/usecase/sign_in.dart
@@ -1,1 +1,25 @@
-class SignInUseCase{}
+import 'package:expense_tracker/features/auth/domain/entity/auth_entities.dart';
+import 'package:expense_tracker/features/auth/domain/repository/auth_repository.dart';
+import 'package:expense_tracker/features/auth/domain/value_object/email.dart';
+import 'package:expense_tracker/features/auth/domain/value_object/password.dart';
+
+class SignInUseCase {
+  final AuthRepository _authRepository;
+  SignInUseCase(this._authRepository);
+
+  Future<AuthEntities> execute(SignInParams params) async {
+    try {
+      return await _authRepository.signInWithEmailAndPassword(
+          email: params.email.value, password: params.password.value);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
+
+class SignInParams {
+  final Email email;
+  final Password password;
+
+  SignInParams({required this.email, required this.password});
+}
