18ca01c73d2113eea3d282a44768dbc63d82da4f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-25 08:31:19 +0700

    feat: add AuthModel for Firebase user integration

---
+++ lib/features/auth/data/model/auth_model.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:expense_tracker/features/auth/domain/entity/auth_entities.dart';
+
+class AuthModel extends AuthEntities {
+  const AuthModel({
+    required super.uuid,
+    required super.email,
+    super.name,
+    super.imageUrl,
+  });
+
+  factory AuthModel.fromFirebaseAuthUser(
+      firebase_auth.User firebaseUser) {
+    return AuthModel(
+        uuid: firebaseUser.uid,
+        email: firebaseUser.email ?? '',
+        name: firebaseUser.displayName,
+        imageUrl: firebaseUser.photoURL);
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'uuid': uuid,
+      'email': email,
+      'name': name,
+      'imageUrl': imageUrl
+    };
+  }
+}
