3db64d40ce51537ae3868b6445cb22ec7dba55fd
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-25 19:33:37 +0700

    feat: implement SignUpUseCase for user registration functionality

--- lib/features/auth/domain/usecase/sign_up.dart
+++ lib/features/auth/domain/usecase/sign_up.dart
@@ -1,1 +1,24 @@
-class SignUpUseCase {}
+import 'package:expense_tracker/features/auth/domain/entity/auth_entities.dart';
+import 'package:expense_tracker/features/auth/domain/repository/auth_repository.dart';
+import 'package:expense_tracker/features/auth/domain/value_object/email.dart';
+import 'package:expense_tracker/features/auth/domain/value_object/password.dart';
+
+class SignUpUseCase {
+  final AuthRepository _authRepository;
+  SignUpUseCase(this._authRepository);
+
+  Future<AuthEntities> execute(SignUpParams params) async {
+    try {
+      return await _authRepository.signUpWithEmailAndPassword(
+          email: params.email.value, password: params.password.value);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
+
+class SignUpParams {
+  final Email email;
+  final Password password;
+  SignUpParams({required this.email, required this.password});
+}
