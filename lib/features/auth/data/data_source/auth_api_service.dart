1e99bde15a54030581637b2e29723db6ee276898
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-25 19:32:57 +0700

    feat: implement authentication methods in AuthApiService for user sign-in, sign-up, and sign-out

--- lib/features/auth/data/data_source/auth_api_service.dart
+++ lib/features/auth/data/data_source/auth_api_service.dart
@@ -1,7 +1,6 @@
 import 'package:expense_tracker/features/auth/data/model/auth_model.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
 
-// TODO make an auth api service
 
 class AuthApiService {
   AuthApiService({firebase_auth.FirebaseAuth? firebaseAuth})
@@ -9,6 +8,15 @@ class AuthApiService {
             firebaseAuth ?? firebase_auth.FirebaseAuth.instance;
 
   final firebase_auth.FirebaseAuth _firebaseAuth;
+
+  Stream<AuthModel?> get user {
+    return _firebaseAuth.authStateChanges().map((firebaseUser) {
+      if (firebaseUser == null) {
+        return null;
+      }
+      return AuthModel.fromFirebaseAuthUser(firebaseUser);
+    });
+  }
 
   Future<AuthModel> signUpWithEmailAndPassword(
       {required String email, required String password}) async {
@@ -22,6 +30,31 @@ class AuthApiService {
             'Sign up failed: The user is null after sign up.');
       }
       return AuthModel.fromFirebaseAuthUser(credential.user!);
+    } catch (e) {
+      throw Exception('An exception has occurred: $e');
+    }
+  }
+
+  Future<AuthModel> signInWithEmailAndPassword(
+      {required String email, required String password}) async {
+    try {
+      firebase_auth.UserCredential credential =
+          await _firebaseAuth.signInWithEmailAndPassword(
+              email: email, password: password);
+
+      if (credential.user == null) {
+        throw Exception(
+            'Sign in failed: The user is null after sign in.');
+      }
+      return AuthModel.fromFirebaseAuthUser(credential.user!);
+    } catch (e) {
+      throw Exception('An exception has occurred: $e');
+    }
+  }
+
+  Future<void> signOut() async {
+    try {
+      await _firebaseAuth.signOut();
     } catch (e) {
       throw Exception('An exception has occurred: $e');
     }
