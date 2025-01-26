4edc9d4248c3cc4545dcae6210ae6dcd622e3df2
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-26 19:58:05 +0700

    feat: add SignUpEvent class and SignUpWithEmailAndPassword event for user sign-up flow

--- lib/features/auth/presentation/bloc/sign_up/sign_up_event.dart
+++ lib/features/auth/presentation/bloc/sign_up/sign_up_event.dart
@@ -1,1 +1,18 @@
-class SignUpEvent {}
+import 'package:equatable/equatable.dart';
+
+class SignUpEvent extends Equatable {
+  const SignUpEvent();
+  @override
+  List<Object?> get props => [];
+}
+
+final class SignUpWithEmailAndPassword extends SignUpEvent {
+  final String email;
+  final String password;
+
+  const SignUpWithEmailAndPassword(
+      {required this.email, required this.password});
+
+  @override
+  List<Object?> get props => [email, password];
+}
