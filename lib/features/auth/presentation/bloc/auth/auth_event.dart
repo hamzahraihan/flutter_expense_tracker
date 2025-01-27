bfac12992eb5fd33dad65b1ad9330d57df1426c4
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-27 15:40:45 +0700

    feat: add AuthEvent class to manage authentication events

---
+++ lib/features/auth/presentation/bloc/auth/auth_event.dart
@@ -0,0 +1,33 @@
+import 'package:equatable/equatable.dart';
+import 'package:expense_tracker/features/auth/domain/entity/auth_entities.dart';
+
+sealed class AuthEvent extends Equatable {
+  const AuthEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+final class Authenticated extends AuthEvent {
+  final AuthEntities user;
+  const Authenticated(this.user);
+
+  @override
+  List<Object?> get props => [user];
+}
+
+final class Unauthenticated extends AuthEvent {
+  const Unauthenticated();
+}
+
+final class AuthLoading extends AuthEvent {
+  const AuthLoading();
+}
+
+final class AuthError extends AuthEvent {
+  final String message;
+  const AuthError(this.message);
+
+  @override
+  List<Object?> get props => [message];
+}
