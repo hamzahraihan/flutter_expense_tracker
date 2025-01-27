3b2e791cb1e3e713adfbe6c62076aee90b322b1a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-27 15:40:56 +0700

    feat: implement SignInScreen as a StatelessWidget with a basic build method

--- lib/features/auth/presentation/screens/sign_in_screen.dart
+++ lib/features/auth/presentation/screens/sign_in_screen.dart
@@ -1,1 +1,11 @@
-class SignInScreen {}
+import 'package:flutter/widgets.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
