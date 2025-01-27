b40f2d5860f15b250bf2e09da11e87c5c29c826c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-27 19:03:12 +0700

    refactor: update AuthStatus enum and extend functionality for improved state management

--- lib/features/auth/presentation/bloc/auth/auth_state.dart
+++ lib/features/auth/presentation/bloc/auth/auth_state.dart
@@ -1,19 +1,27 @@
 import 'package:equatable/equatable.dart';
 import 'package:expense_tracker/features/auth/domain/entity/auth_entities.dart';
 
-enum AuthStatusX { authenticated, unauthenticated, loading }
+enum AuthStatus { authenticated, unauthenticated, loading, failure }
+
+extension AuthStatusX on AuthStatus {
+  bool get isAuthenticated => this == AuthStatus.authenticated;
+  bool get isLoading => this == AuthStatus.loading;
+  bool get isUnauthenticated => this == AuthStatus.unauthenticated;
+  bool get isFailure => this == AuthStatus.failure;
+}
 
 class AuthState extends Equatable {
   final AuthEntities? user;
   final String? message;
-  final AuthStatusX? authStatus;
+  final AuthStatus? authStatus;
 
-  const AuthState({this.user, this.message, this.authStatus});
+  const AuthState(
+      {this.user,
+      this.message,
+      this.authStatus = AuthStatus.unauthenticated});
 
   AuthState copyWith(
-      {AuthEntities? user,
-      String? message,
-      AuthStatusX? authStatus}) {
+      {AuthEntities? user, String? message, AuthStatus? authStatus}) {
     return AuthState(
         user: user ?? this.user,
         message: message ?? this.message,
