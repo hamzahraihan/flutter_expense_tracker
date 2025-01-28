7480e419e294c5942cdddac6b590998dc29d6672
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-28 10:36:43 +0700

    feat: implement Google Sign-In functionality in AuthApiService

--- lib/features/auth/data/data_source/auth_api_service.dart
+++ lib/features/auth/data/data_source/auth_api_service.dart
@@ -1,13 +1,21 @@
 import 'package:expense_tracker/features/auth/data/model/auth_model.dart';
 import 'package:firebase_auth/firebase_auth.dart' as firebase_auth;
+import 'package:flutter/foundation.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthApiService {
-  AuthApiService({firebase_auth.FirebaseAuth? firebaseAuth})
+  AuthApiService(
+      {firebase_auth.FirebaseAuth? firebaseAuth,
+      GoogleSignIn? googleSignIn})
       : _firebaseAuth =
-            firebaseAuth ?? firebase_auth.FirebaseAuth.instance;
+            firebaseAuth ?? firebase_auth.FirebaseAuth.instance,
+        _googleSignIn = googleSignIn ?? GoogleSignIn.standard();
 
   final firebase_auth.FirebaseAuth _firebaseAuth;
+  final GoogleSignIn _googleSignIn;
 
+  /// Stream of [User] which will emit the current user when
+  /// the authentication state changes.
   Stream<AuthModel?> get user {
     return _firebaseAuth.authStateChanges().map((firebaseUser) {
       if (firebaseUser == null) {
@@ -48,6 +56,35 @@ class AuthApiService {
       return AuthModel.fromFirebaseAuthUser(credential.user!);
     } catch (e) {
       throw Exception('An exception has occurred: $e');
+    }
+  }
+
+  /// Starts the Sign In with Google Flow.
+  Future<void> signInWithGoogle() async {
+    try {
+      late final firebase_auth.AuthCredential credential;
+
+      if (kIsWeb) {
+        final firebase_auth.GoogleAuthProvider googleProvider =
+            firebase_auth.GoogleAuthProvider();
+        final firebase_auth.UserCredential userCredential =
+            await _firebaseAuth.signInWithPopup(googleProvider);
+
+        credential = userCredential.credential!;
+      } else {
+        final GoogleSignInAccount? googleUser =
+            await _googleSignIn.signIn();
+        final GoogleSignInAuthentication googleAuth =
+            await googleUser!.authentication;
+
+        credential = firebase_auth.GoogleAuthProvider.credential(
+            accessToken: googleAuth.accessToken,
+            idToken: googleAuth.idToken);
+      }
+
+      await _firebaseAuth.signInWithCredential(credential);
+    } catch (e) {
+      throw Exception('Google login has failed: $e');
     }
   }
 
