eb0a87b9c7ae5737517b9aefab98223c911b7f86
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-29 09:53:06 +0700

    fix: rename uuid to uid in AuthEntities for consistency

--- lib/features/auth/domain/entity/auth_entities.dart
+++ lib/features/auth/domain/entity/auth_entities.dart
@@ -1,23 +1,23 @@
 import 'package:equatable/equatable.dart';
 
 class AuthEntities extends Equatable {
-  final String uuid;
+  final String uid;
   final String email;
   final String? name;
   final String? imageUrl;
 
   const AuthEntities({
-    required this.uuid,
+    required this.uid,
     required this.email,
     this.name,
     this.imageUrl,
   });
 
   static const AuthEntities empty =
-      AuthEntities(uuid: '', email: '', name: '', imageUrl: '');
+      AuthEntities(uid: '', email: '', name: '', imageUrl: '');
 
   bool get isEmpty => this == AuthEntities.empty;
 
   @override
-  List<Object?> get props => [uuid, email, name, imageUrl];
+  List<Object?> get props => [uid, email, name, imageUrl];
 }
