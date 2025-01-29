b45c2cc1d8d57611180385c2345e0af366f41a3e
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-29 15:22:16 +0700

    feat: enhance SignInScreen with debounced email input and validation feedback

--- lib/features/auth/presentation/screens/sign_in_screen.dart
+++ lib/features/auth/presentation/screens/sign_in_screen.dart
@@ -1,13 +1,32 @@
+import 'dart:async';
+
 import 'package:expense_tracker/features/auth/presentation/bloc/auth/auth_bloc.dart';
 import 'package:expense_tracker/features/auth/presentation/bloc/auth/auth_state.dart';
 import 'package:expense_tracker/features/auth/presentation/bloc/sign_in/sign_in_bloc.dart';
 import 'package:expense_tracker/features/auth/presentation/bloc/sign_in/sign_in_event.dart';
+import 'package:expense_tracker/features/auth/presentation/bloc/sign_in/sign_in_state.dart';
+import 'package:expense_tracker/features/auth/presentation/bloc/status.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class SignInScreen extends StatelessWidget {
+class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
+
+  static const String routeName = '/sign-in';
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  Timer? debounce;
+
+  @override
+  void dispose() {
+    debounce?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,24 +38,32 @@ class SignInScreen extends StatelessWidget {
             ..showSnackBar(const SnackBar(
                 content: Text('Authentication failure')));
         }
-        return _buildBody(context, state);
+        return _buildBody(context, state, debounce);
       },
     );
   }
 
-  Scaffold _buildBody(BuildContext context, AuthState state) {
+  Scaffold _buildBody(
+      BuildContext context, AuthState state, debounce) {
+    SignInState signInState =
+        context.select((SignInBloc bloc) => bloc.state);
+
     return Scaffold(
         body: Form(
+            child: Container(
+      margin: const EdgeInsets.symmetric(horizontal: 12),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-        spacing: 20,
+        spacing: 12,
         children: [
+          const Icon(FontAwesomeIcons.wallet),
           TextFormField(
             onChanged: (value) {
-              // context
-              //     .read<AddTransactionBloc>()
-              //     .add(AddTransactionDescriptionChanged(value));
+              if (debounce?.isActive ?? false) debounce?.cancel();
+              debounce = Timer(const Duration(milliseconds: 500), () {
+                context.read<SignInBloc>().add(EmailChanged(value));
+              });
             },
             validator: (value) {
               if (value == null || value.isEmpty) {
@@ -44,10 +71,38 @@ class SignInScreen extends StatelessWidget {
               }
               return null;
             },
-            maxLines: 1,
-            maxLength: 64,
             decoration: InputDecoration(
-              hintText: 'Description',
+              hintText: 'Email',
+              errorText:
+                  signInState.emailStatus == EmailStatus.invalid
+                      ? 'invalid email'
+                      : null,
+              border: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.black26),
+                  borderRadius: BorderRadius.circular(16)),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.black26),
+                  borderRadius: BorderRadius.circular(16)),
+            ),
+          ),
+          TextFormField(
+            key: const Key('signIn_passwordInput_textField'),
+            obscureText: true,
+            onChanged: (value) {
+              context.read<SignInBloc>().add(PasswordChanged(value));
+            },
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter password';
+              }
+              return null;
+            },
+            decoration: InputDecoration(
+              hintText: 'Password',
+              errorText:
+                  signInState.passwordStatus == PasswordStatus.invalid
+                      ? 'invalid password'
+                      : null,
               border: OutlineInputBorder(
                   borderSide: const BorderSide(color: Colors.black26),
                   borderRadius: BorderRadius.circular(16)),
@@ -57,17 +112,42 @@ class SignInScreen extends StatelessWidget {
             ),
           ),
           ElevatedButton.icon(
+            key: const Key('loginWithCredential'),
+            label: const Text(
+              'SIGN IN',
+              style: TextStyle(color: Colors.white),
+            ),
+            style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                backgroundColor: Colors.blueAccent,
+                minimumSize: const Size(double.infinity, 50)),
+            icon: const Icon(Icons.login, color: Colors.white),
+            onPressed: () {
+              context
+                  .read<SignInBloc>()
+                  .add(const SignInWithEmailAndPassword());
+            },
+          ),
+          const Divider(),
+          const Text(
+            'Or with',
+            style: TextStyle(
+                color: Colors.black38, fontWeight: FontWeight.bold),
+          ),
+          ElevatedButton.icon(
             key: const Key('loginForm_googleLogin_raisedButton'),
             label: const Text(
               'SIGN IN WITH GOOGLE',
               style: TextStyle(color: Colors.white),
             ),
             style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              backgroundColor: Colors.blueAccent,
-            ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                backgroundColor: Colors.blueAccent,
+                minimumSize: const Size(double.infinity, 50)),
             icon: const Icon(FontAwesomeIcons.google,
                 color: Colors.white),
             onPressed: () => context
@@ -76,6 +156,6 @@ class SignInScreen extends StatelessWidget {
           )
         ],
       ),
-    ));
+    )));
   }
 }
