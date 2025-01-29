3eac0ae4fd284bbfccf4b6c356d4994e54408eb9
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-29 09:53:15 +0700

    refactor: streamline authentication request handling in AuthBloc

--- lib/features/auth/presentation/bloc/auth/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth/auth_bloc.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:expense_tracker/features/auth/data/repository/auth_repository_impl.dart';
 import 'package:expense_tracker/features/auth/domain/entity/auth_entities.dart';
 import 'package:expense_tracker/features/auth/presentation/bloc/auth/auth_event.dart';
@@ -8,30 +6,15 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final AuthRepositoryImpl _authRepositoryImpl;
-  StreamSubscription? _authSubscription;
 
-  AuthBloc(this._authRepositoryImpl) : super(const AuthState()) {
+  AuthBloc(this._authRepositoryImpl) : super(AuthState()) {
     on<Authenticated>(_onAuthenticateRequest);
   }
 
-  void _onAuthenticateRequest(
-      Authenticated event, Emitter<AuthState> emit) async {
-    emit(state.copyWith(authStatus: AuthStatus.loading));
-    try {
-      // cancel any existing subscription
-      _authSubscription?.cancel();
-
-      final AuthEntities? authUser =
-          await _authRepositoryImpl.authUser.first;
-
-      if (authUser == null) {
-        emit(state.copyWith(authStatus: AuthStatus.authenticated));
-      } else {
-        emit(state.copyWith(
-            user: authUser, authStatus: AuthStatus.authenticated));
-      }
-    } catch (e) {
-      emit(state.copyWith(authStatus: AuthStatus.unauthenticated));
-    }
+  Future<void> _onAuthenticateRequest(
+      Authenticated event, Emitter<AuthState> emit) {
+    return emit.onEach<AuthEntities>(_authRepositoryImpl.authUser,
+        onData: (user) => emit(AuthState(user: user)),
+        onError: addError);
   }
 }
