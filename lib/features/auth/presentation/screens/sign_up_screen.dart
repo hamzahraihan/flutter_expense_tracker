878ebf6314c6e91ba905484ddac09b1b1aaf9277
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-29 15:22:21 +0700

    feat: implement SignUpScreen with basic layout and routing

--- lib/features/auth/presentation/screens/sign_up_screen.dart
+++ lib/features/auth/presentation/screens/sign_up_screen.dart
@@ -1,1 +1,15 @@
-class SignUpScreen {}
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+  static const String routeName = '/sign-up';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('sign up screen'),
+      ),
+    );
+  }
+}
