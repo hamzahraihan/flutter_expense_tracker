0b6dc7414d6d0734d369b55c66754e032cb60343
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-01-30 14:54:44 +0700

    feat: enhance SignInScreen layout with improved structure and additional sign-up prompt

--- lib/features/auth/presentation/screens/sign_in_screen.dart
+++ lib/features/auth/presentation/screens/sign_in_screen.dart
@@ -49,113 +49,150 @@ class _SignInScreenState extends State<SignInScreen> {
         context.select((SignInBloc bloc) => bloc.state);
 
     return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'Sign In',
+            style: TextStyle(fontSize: 18.0),
+          ),
+        ),
         body: Form(
             child: Container(
-      margin: const EdgeInsets.symmetric(horizontal: 12),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        spacing: 12,
-        children: [
-          const Icon(FontAwesomeIcons.wallet),
-          TextFormField(
-            onChanged: (value) {
-              if (debounce?.isActive ?? false) debounce?.cancel();
-              debounce = Timer(const Duration(milliseconds: 500), () {
-                context.read<SignInBloc>().add(EmailChanged(value));
-              });
-            },
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter description';
-              }
-              return null;
-            },
-            decoration: InputDecoration(
-              hintText: 'Email',
-              errorText:
-                  signInState.emailStatus == EmailStatus.invalid
-                      ? 'invalid email'
-                      : null,
-              border: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.black26),
-                  borderRadius: BorderRadius.circular(16)),
-              enabledBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.black26),
-                  borderRadius: BorderRadius.circular(16)),
-            ),
-          ),
-          TextFormField(
-            key: const Key('signIn_passwordInput_textField'),
-            obscureText: true,
-            onChanged: (value) {
-              context.read<SignInBloc>().add(PasswordChanged(value));
-            },
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter password';
-              }
-              return null;
-            },
-            decoration: InputDecoration(
-              hintText: 'Password',
-              errorText:
-                  signInState.passwordStatus == PasswordStatus.invalid
+          margin: const EdgeInsets.symmetric(horizontal: 12),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            spacing: 12,
+            children: [
+              TextFormField(
+                onChanged: (value) {
+                  if (debounce?.isActive ?? false) debounce?.cancel();
+                  debounce =
+                      Timer(const Duration(milliseconds: 500), () {
+                    context
+                        .read<SignInBloc>()
+                        .add(EmailChanged(value));
+                  });
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter description';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  errorText:
+                      signInState.emailStatus == EmailStatus.invalid
+                          ? 'invalid email'
+                          : null,
+                  border: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.black26),
+                      borderRadius: BorderRadius.circular(16)),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.black26),
+                      borderRadius: BorderRadius.circular(16)),
+                ),
+              ),
+              TextFormField(
+                key: const Key('signIn_passwordInput_textField'),
+                obscureText: true,
+                onChanged: (value) {
+                  context
+                      .read<SignInBloc>()
+                      .add(PasswordChanged(value));
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter password';
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  errorText: signInState.passwordStatus ==
+                          PasswordStatus.invalid
                       ? 'invalid password'
                       : null,
-              border: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.black26),
-                  borderRadius: BorderRadius.circular(16)),
-              enabledBorder: OutlineInputBorder(
-                  borderSide: const BorderSide(color: Colors.black26),
-                  borderRadius: BorderRadius.circular(16)),
-            ),
-          ),
-          ElevatedButton.icon(
-            key: const Key('loginWithCredential'),
-            label: const Text(
-              'SIGN IN',
-              style: TextStyle(color: Colors.white),
-            ),
-            style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
+                  border: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.black26),
+                      borderRadius: BorderRadius.circular(16)),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          const BorderSide(color: Colors.black26),
+                      borderRadius: BorderRadius.circular(16)),
                 ),
-                backgroundColor: Colors.blueAccent,
-                minimumSize: const Size(double.infinity, 50)),
-            icon: const Icon(Icons.login, color: Colors.white),
-            onPressed: () {
-              context
-                  .read<SignInBloc>()
-                  .add(const SignInWithEmailAndPassword());
-            },
-          ),
-          const Divider(),
-          const Text(
-            'Or with',
-            style: TextStyle(
-                color: Colors.black38, fontWeight: FontWeight.bold),
-          ),
-          ElevatedButton.icon(
-            key: const Key('loginForm_googleLogin_raisedButton'),
-            label: const Text(
-              'SIGN IN WITH GOOGLE',
-              style: TextStyle(color: Colors.white),
-            ),
-            style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
+              ),
+              ElevatedButton.icon(
+                key: const Key('loginWithCredential'),
+                label: const Text(
+                  'SIGN IN',
+                  style: TextStyle(color: Colors.white),
                 ),
-                backgroundColor: Colors.blueAccent,
-                minimumSize: const Size(double.infinity, 50)),
-            icon: const Icon(FontAwesomeIcons.google,
-                color: Colors.white),
-            onPressed: () => context
-                .read<SignInBloc>()
-                .add(const SignInWithGoogle()),
-          )
-        ],
-      ),
-    )));
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    backgroundColor: Colors.blueAccent,
+                    minimumSize: const Size(double.infinity, 50)),
+                icon: const Icon(Icons.login, color: Colors.white),
+                onPressed: () {
+                  context
+                      .read<SignInBloc>()
+                      .add(const SignInWithEmailAndPassword());
+                },
+              ),
+              const Divider(),
+              const Text(
+                'Or with',
+                style: TextStyle(
+                    color: Colors.black38,
+                    fontWeight: FontWeight.bold),
+              ),
+              ElevatedButton.icon(
+                key: const Key('loginForm_googleLogin_raisedButton'),
+                label: const Text(
+                  'SIGN IN WITH GOOGLE',
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    backgroundColor: Colors.blueAccent,
+                    minimumSize: const Size(double.infinity, 50)),
+                icon: const Icon(FontAwesomeIcons.google,
+                    color: Colors.white),
+                onPressed: () => context
+                    .read<SignInBloc>()
+                    .add(const SignInWithGoogle()),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 0,
+                children: [
+                  const Text(
+                    'Don’t have an account yet?',
+                    style: TextStyle(
+                        color: Colors.black38,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  TextButton(
+                      onPressed: () => {},
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                            color: Colors.blue,
+                            decoration: TextDecoration.underline,
+                            fontWeight: FontWeight.bold),
+                      ))
+                ],
+              )
+            ],
+          ),
+        )));
   }
 }
