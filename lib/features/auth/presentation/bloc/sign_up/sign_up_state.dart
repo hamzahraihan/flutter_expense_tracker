65d7ce8d892b8f0320ce3fbf277c502bfd38801a
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-01 14:19:39 +0700

    feat: enhance SignUpState with Equatable and form status management

--- lib/features/auth/presentation/bloc/sign_up/sign_up_state.dart
+++ lib/features/auth/presentation/bloc/sign_up/sign_up_state.dart
@@ -1,1 +1,48 @@
-class SignUpState {}
+import 'package:equatable/equatable.dart';
+import 'package:expense_tracker/features/auth/domain/value_object/email.dart';
+import 'package:expense_tracker/features/auth/domain/value_object/password.dart';
+import 'package:expense_tracker/features/auth/presentation/bloc/status.dart';
+
+extension FormStatusX on FormStatus {
+  bool get initial => this == FormStatus.initial;
+  bool get invalid => this == FormStatus.invalid;
+  bool get submissionInProgress =>
+      this == FormStatus.submissionInProgress;
+  bool get submissionSuccess => this == FormStatus.submissionSuccess;
+  bool get submissionFailure => this == FormStatus.submissionFailure;
+}
+
+class SignUpState extends Equatable {
+  final Email? email;
+  final Password? password;
+  final EmailStatus emailStatus;
+  final FormStatus formStatus;
+  final PasswordStatus passwordStatus;
+
+  const SignUpState({
+    this.email,
+    this.password,
+    this.emailStatus = EmailStatus.unknown,
+    this.passwordStatus = PasswordStatus.unknown,
+    this.formStatus = FormStatus.initial,
+  });
+
+  SignUpState copyWith({
+    Email? email,
+    Password? password,
+    EmailStatus? emailStatus,
+    PasswordStatus? passwordStatus,
+    FormStatus? formStatus,
+  }) {
+    return SignUpState(
+        email: email ?? this.email,
+        password: password ?? this.password,
+        emailStatus: emailStatus ?? this.emailStatus,
+        passwordStatus: passwordStatus ?? this.passwordStatus,
+        formStatus: formStatus ?? this.formStatus);
+  }
+
+  @override
+  List<Object?> get props =>
+      [email, password, emailStatus, passwordStatus, formStatus];
+}
