a8184b3b1cb7fbcd04391eabfee7607f5d724dff
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-01 19:42:10 +0700

    feat: add name and usernameStatus fields to SignUpState for enhanced user data management

--- lib/features/auth/presentation/bloc/sign_up/sign_up_state.dart
+++ lib/features/auth/presentation/bloc/sign_up/sign_up_state.dart
@@ -13,36 +13,51 @@ extension FormStatusX on FormStatus {
 }
 
 class SignUpState extends Equatable {
+  final String? name;
   final Email? email;
   final Password? password;
   final EmailStatus emailStatus;
+  final UsernameStatus usernameStatus;
   final FormStatus formStatus;
   final PasswordStatus passwordStatus;
 
   const SignUpState({
+    this.name,
     this.email,
     this.password,
+    this.usernameStatus = UsernameStatus.unknown,
     this.emailStatus = EmailStatus.unknown,
     this.passwordStatus = PasswordStatus.unknown,
     this.formStatus = FormStatus.initial,
   });
 
   SignUpState copyWith({
+    String? name,
     Email? email,
     Password? password,
+    UsernameStatus? usernameStatus,
     EmailStatus? emailStatus,
     PasswordStatus? passwordStatus,
     FormStatus? formStatus,
   }) {
     return SignUpState(
+        name: name ?? this.name,
         email: email ?? this.email,
         password: password ?? this.password,
+        usernameStatus: usernameStatus ?? this.usernameStatus,
         emailStatus: emailStatus ?? this.emailStatus,
         passwordStatus: passwordStatus ?? this.passwordStatus,
         formStatus: formStatus ?? this.formStatus);
   }
 
   @override
-  List<Object?> get props =>
-      [email, password, emailStatus, passwordStatus, formStatus];
+  List<Object?> get props => [
+        name,
+        email,
+        password,
+        usernameStatus,
+        emailStatus,
+        passwordStatus,
+        formStatus
+      ];
 }
