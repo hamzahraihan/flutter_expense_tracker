ec11c2a505c9e8314a603cad71a29445334ddb00
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-05 14:06:08 +0700

    feat: update GetAccountWalletUseCase to use TransactionRepositoryImpl

--- lib/features/expense/domain/usecase/get_account_wallet.dart
+++ lib/features/expense/domain/usecase/get_account_wallet.dart
@@ -1,11 +1,11 @@
 import 'package:expense_tracker/features/expense/data/model/account_wallet_model.dart';
-import 'package:expense_tracker/features/expense/domain/repository/transaction_repository.dart';
+import 'package:expense_tracker/features/expense/data/repository/transaction_repository_impl.dart';
 
 class GetAccountWalletUseCase {
-  final TransactionRepository _transactionRepository;
-  GetAccountWalletUseCase(this._transactionRepository);
+  final TransactionRepositoryImpl _transactionRepositoryImpl;
+  GetAccountWalletUseCase(this._transactionRepositoryImpl);
 
   Future<List<AccountWalletModel>> execute() {
-    return _transactionRepository.getAccountWallet();
+    return _transactionRepositoryImpl.getAccountWallet();
   }
 }
