b075242fdef55cb965ec00aa16f2cf9b165cbbf7
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-05 08:54:36 +0700

    feat: add AccountEvent class and related events for account wallet management

---
+++ lib/features/expense/presentation/bloc/account_wallet/account_event.dart
@@ -0,0 +1,26 @@
+import 'package:equatable/equatable.dart';
+
+sealed class AccountEvent extends Equatable {
+  const AccountEvent();
+  @override
+  List<Object?> get props => [];
+}
+
+final class GetAccountWallet extends AccountEvent {
+  const GetAccountWallet();
+}
+
+final class AccountBalanceChanged extends AccountEvent {
+  final int balance;
+  const AccountBalanceChanged(this.balance);
+  @override
+  List<Object?> get props => [balance];
+}
+
+final class AccountTypeChanged extends AccountEvent {
+  final String accountWalletType;
+  const AccountTypeChanged(this.accountWalletType);
+
+  @override
+  List<Object?> get props => [accountWalletType];
+}
