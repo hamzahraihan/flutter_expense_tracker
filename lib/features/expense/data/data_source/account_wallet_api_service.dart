a760bf0d8ee70b6ed495eef3753c8cd7af000b85
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-05 08:33:09 +0700

    feat: implement AccountWalletApiService for account wallet data management

---
+++ lib/features/expense/data/data_source/account_wallet_api_service.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:expense_tracker/core/constants/constants.dart';
+import 'package:expense_tracker/features/expense/data/model/account_wallet_model.dart';
+import 'package:expense_tracker/services/firebase.dart';
+
+class AccountWalletApiService {
+  Future<List<AccountWalletModel>> getAccountWallet() async {
+    try {
+      final QuerySnapshot<Map<String, dynamic>> snapshot =
+          await db.collection('accounts').get();
+      return snapshot.docs
+          .map((doc) => AccountWalletModel.fromFirestore(doc, null))
+          .toList();
+    } catch (e) {
+      throw Exception('Error fetching transaction: $e');
+    }
+  }
+
+  Future<void> addAccountWallet(
+      Map<String, dynamic> accountWallet) async {
+    try {
+      await db
+          .collection(accountWalletCollectionPath)
+          .doc()
+          .set(accountWallet);
+    } catch (e) {
+      throw Exception('Error fetching transaction: $e');
+    }
+  }
+}
