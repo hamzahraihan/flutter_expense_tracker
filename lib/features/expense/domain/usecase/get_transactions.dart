dff429e363abc592c1fef3a2c7e480c2cb403c70
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-07 08:58:39 +0700

    feat: update GetTransactionsUseCase to use AuthUserEntities for user authentication

--- lib/features/expense/domain/usecase/get_transactions.dart
+++ lib/features/expense/domain/usecase/get_transactions.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/features/auth/domain/entity/auth_user_entities.dart';
 import 'package:expense_tracker/features/expense/data/model/transactions_model.dart';
 import 'package:expense_tracker/features/expense/data/repository/transaction_repository_impl.dart';
 
@@ -5,7 +6,7 @@ class GetTransactionsUseCase {
   final TransactionRepositoryImpl _transactionRepositoryImpl;
   GetTransactionsUseCase(this._transactionRepositoryImpl);
 
-  Future<List<TransactionsModel>> execute(authUser) {
+  Future<List<TransactionsModel>> execute(AuthUserEntities authUser) {
     return _transactionRepositoryImpl.getTransactions(authUser);
   }
 }
