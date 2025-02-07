576b042930e2148b471468773886178d615d6d92
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-07 08:57:40 +0700

    feat: rename AuthEntities to AuthUserEntities and update related references

--- lib/features/auth/domain/entity/auth_user_entities.dart
+++ lib/features/auth/domain/entity/auth_user_entities.dart
@@ -1,22 +1,22 @@
 import 'package:equatable/equatable.dart';
 
-class AuthEntities extends Equatable {
+class AuthUserEntities extends Equatable {
   final String uid;
   final String email;
   final String? name;
   final String? imageUrl;
 
-  const AuthEntities({
+  const AuthUserEntities({
     required this.uid,
     required this.email,
     this.name,
     this.imageUrl,
   });
 
-  static const AuthEntities empty =
-      AuthEntities(uid: '', email: '', name: '', imageUrl: '');
+  static const AuthUserEntities empty =
+      AuthUserEntities(uid: '', email: '', name: '', imageUrl: '');
 
-  bool get isEmpty => this == AuthEntities.empty;
+  bool get isEmpty => this == AuthUserEntities.empty;
 
   @override
   List<Object?> get props => [uid, email, name, imageUrl];
