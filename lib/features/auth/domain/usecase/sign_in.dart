89ae083bc202a3663a8d8d04041ffb65640fea1b
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-07 08:57:51 +0700

    feat: update SignInUseCase to use AuthUserEntities for user credential handling

--- lib/features/auth/domain/usecase/sign_in.dart
+++ lib/features/auth/domain/usecase/sign_in.dart
@@ -1,4 +1,4 @@
-import 'package:expense_tracker/features/auth/domain/entity/auth_entities.dart';
+import 'package:expense_tracker/features/auth/domain/entity/auth_user_entities.dart';
 import 'package:expense_tracker/features/auth/domain/repository/auth_repository.dart';
 import 'package:expense_tracker/features/auth/domain/value_object/email.dart';
 import 'package:expense_tracker/features/auth/domain/value_object/password.dart';
@@ -8,7 +8,7 @@ class SignInUseCase {
   final AuthRepository _authRepository;
   SignInUseCase(this._authRepository);
 
-  Future<AuthEntities> withCredential(SignInParams params) async {
+  Future<AuthUserEntities> withCredential(SignInParams params) async {
     try {
       return await _authRepository.signInWithEmailAndPassword(
           email: params.email.value, password: params.password.value);
