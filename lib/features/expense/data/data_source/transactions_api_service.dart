fac467e22933a35e5e33b38bf6127e17c86b674c
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-07 08:58:24 +0700

    feat: update TransactionsApiService to use AuthUserEntities for user authentication

--- lib/features/expense/data/data_source/transactions_api_service.dart
+++ lib/features/expense/data/data_source/transactions_api_service.dart
@@ -1,22 +1,37 @@
+import 'dart:developer';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:expense_tracker/core/constants/constants.dart';
-import 'package:expense_tracker/features/auth/domain/entity/auth_entities.dart';
+import 'package:expense_tracker/features/auth/domain/entity/auth_user_entities.dart';
 import 'package:expense_tracker/features/expense/data/model/account_wallet_model.dart';
 import 'package:expense_tracker/features/expense/data/model/transactions_model.dart';
 import 'package:expense_tracker/services/firebase.dart';
 
 class TransactionsApiService {
   Future<List<TransactionsModel>> getTransactions(
-      AuthEntities authUser) async {
+      AuthUserEntities authUser) async {
     try {
-      final QuerySnapshot<Map<String, dynamic>> snapshot = await db
+      log('User ID: ${authUser.uid}');
+
+      final query = db
           .collection(transactionCollectionPath)
-          .where('uid', isEqualTo: authUser.uid)
-          .get();
-      return snapshot.docs
-          .map((doc) => TransactionsModel.fromFirestore(doc, null))
-          .toList();
+          .where('uid', isEqualTo: authUser.uid);
+
+      log('Query: ${query.parameters}');
+
+      final snapshot = await query.get();
+      log('Documents found: ${snapshot.docs.length}');
+
+      if (snapshot.docs.isEmpty) {
+        log('No documents found for user ${authUser.uid}');
+        return [];
+      } else {
+        return snapshot.docs
+            .map((doc) => TransactionsModel.fromFirestore(doc, null))
+            .toList();
+      }
     } catch (e) {
+      log('Error: $e');
       throw Exception('Error fetching transaction: $e');
     }
   }
