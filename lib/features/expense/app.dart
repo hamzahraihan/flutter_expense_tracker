9bd24add68e1772d238e1741478f725485b36e99
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-07 08:58:19 +0700

    feat: update ExpenseTrackerApp to use AuthUserEntities for user authentication

--- lib/features/expense/app.dart
+++ lib/features/expense/app.dart
@@ -1,6 +1,6 @@
 import 'dart:developer';
 
-import 'package:expense_tracker/features/auth/domain/entity/auth_entities.dart';
+import 'package:expense_tracker/features/auth/domain/entity/auth_user_entities.dart';
 import 'package:expense_tracker/features/expense/presentation/screens/account/account_screen.dart';
 import 'package:expense_tracker/features/expense/presentation/screens/home/home_screen.dart';
 import 'package:expense_tracker/features/expense/presentation/screens/transactions/transactions_screen.dart';
@@ -11,7 +11,7 @@ class ExpenseTrackerApp extends StatefulWidget {
       {super.key,
       required this.initialIndex,
       required this.authUser});
-  final AuthEntities authUser;
+  final AuthUserEntities authUser;
   final int initialIndex;
   @override
   State<ExpenseTrackerApp> createState() => _ExpenseTrackerAppState();
@@ -38,7 +38,7 @@ class _ExpenseTrackerAppState extends State<ExpenseTrackerApp> {
 
   @override
   Widget build(BuildContext context) {
-    log('from main screen ${widget.authUser.toString()}');
+    log('from main screen ${widget.authUser}');
     final List<NavigationDestination> navButton = [
       const NavigationDestination(
           selectedIcon: Icon(
