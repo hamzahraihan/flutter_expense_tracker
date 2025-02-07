d7171d42a16f614550499b623c37a7d1ee9a85fa
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-07 08:58:02 +0700

    feat: update AuthBloc to use AuthUserEntities for user authentication state management

--- lib/features/auth/presentation/bloc/auth/auth_bloc.dart
+++ lib/features/auth/presentation/bloc/auth/auth_bloc.dart
@@ -1,7 +1,7 @@
 import 'dart:developer';
 
 import 'package:expense_tracker/features/auth/data/repository/auth_repository_impl.dart';
-import 'package:expense_tracker/features/auth/domain/entity/auth_entities.dart';
+import 'package:expense_tracker/features/auth/domain/entity/auth_user_entities.dart';
 import 'package:expense_tracker/features/auth/presentation/bloc/auth/auth_event.dart';
 import 'package:expense_tracker/features/auth/presentation/bloc/auth/auth_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -16,16 +16,16 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
 
   Future<void> _onAuthenticateRequest(
       Authenticated event, Emitter<AuthState> emit) async {
-    log('from auth bloc ${_authRepositoryImpl.authUser.toString()}');
-    return await emit.onEach<AuthEntities>(
-        _authRepositoryImpl.authUser,
-        onData: (user) => emit(AuthState(user: user)),
-        onError: addError);
+    await emit.onEach<AuthUserEntities?>(_authRepositoryImpl.authUser,
+        onData: (AuthUserEntities? user) {
+      log('from auth bloc $user');
+      emit(AuthState(user: user ?? AuthUserEntities.empty));
+    }, onError: addError);
   }
 
   Future<void> _onSignOutPressed(
       SignOutPressed event, Emitter<AuthState> emit) async {
     await _authRepositoryImpl.signOut();
-    emit(AuthState(user: AuthEntities.empty));
+    emit(AuthState(user: AuthUserEntities.empty));
   }
 }
