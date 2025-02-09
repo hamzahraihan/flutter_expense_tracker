50fe39a33cc31cad2535bee494c5c668f9f23b16
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-09 16:35:48 +0700

    refactor: remove AddExpenseUseCase and related event handling from TransactionFirebaseBloc

--- lib/features/expense/presentation/bloc/transaction/firebase/transaction_firebase_bloc.dart
+++ lib/features/expense/presentation/bloc/transaction/firebase/transaction_firebase_bloc.dart
@@ -5,7 +5,7 @@ import 'package:bloc/bloc.dart';
 import 'package:expense_tracker/features/auth/domain/entity/auth_user_entities.dart';
 import 'package:expense_tracker/features/expense/data/model/transactions_model.dart';
 import 'package:expense_tracker/features/expense/data/repository/transaction_repository_impl.dart';
-import 'package:expense_tracker/features/expense/domain/usecase/add_expense.dart';
+
 import 'package:expense_tracker/features/expense/domain/usecase/get_transactions.dart';
 import 'package:expense_tracker/features/expense/presentation/bloc/transaction/firebase/transaction_firebase_event.dart';
 import 'package:expense_tracker/features/expense/presentation/bloc/transaction/firebase/transaction_firebase_state.dart';
@@ -15,7 +15,6 @@ class TransactionFirebaseBloc
   final TransactionRepositoryImpl _transactionRepositoryImpl;
 
   // get user id
-  final Stream<AuthUserEntities> authUser;
   late AuthUserEntities _currentUser;
   late final StreamSubscription<AuthUserEntities>
       _authUserSubscription;
@@ -23,11 +22,8 @@ class TransactionFirebaseBloc
   late final GetTransactionsUseCase _getTransactionsUseCase =
       GetTransactionsUseCase(_transactionRepositoryImpl);
 
-  late final AddExpenseUseCase _addExpenseUseCase =
-      AddExpenseUseCase(_transactionRepositoryImpl);
-
-  TransactionFirebaseBloc(
-      this._transactionRepositoryImpl, this.authUser)
+  TransactionFirebaseBloc(this._transactionRepositoryImpl,
+      Stream<AuthUserEntities> authUser)
       : super(const TransactionFirebaseState()) {
     // handle the Stream of AuthUserEntities authUser
     _authUserSubscription = authUser.listen((AuthUserEntities user) {
@@ -36,7 +32,6 @@ class TransactionFirebaseBloc
       add(const GetTransaction()); // refresh data when user changes
     });
     on<GetTransaction>(_onFetchTransactions);
-    on<AddExpenseTransaction>(_onAddExpenseTransaction);
     on<ClearTransactions>(_onClearTransactionData);
   }
 
@@ -50,9 +45,7 @@ class TransactionFirebaseBloc
       Emitter<TransactionFirebaseState> emit) async {
     log('from fetch transaction bloc ${_currentUser.toString()}');
 
-    emit(state.copyWith(status: TransactionStatus.loading));
-
-// First check if we have a valid user
+    // First check if we have a valid user
     if (_currentUser.isEmpty) {
       emit(state.copyWith(
           status: TransactionStatus.success,
@@ -109,19 +102,5 @@ class TransactionFirebaseBloc
       Emitter<TransactionFirebaseState> emit) {
     log('transaction cleared');
     emit(state.copyWith(transactions: []));
-  }
-
-  void _onAddExpenseTransaction(AddExpenseTransaction event,
-      Emitter<TransactionFirebaseState> emit) async {
-    emit(state.copyWith(status: TransactionStatus.loading));
-    try {
-      await _addExpenseUseCase.execute(event.transaction);
-
-      add(const GetTransaction());
-
-      emit(state.copyWith(status: TransactionStatus.success));
-    } catch (e) {
-      emit(state.copyWith(status: TransactionStatus.failure));
-    }
   }
 }
