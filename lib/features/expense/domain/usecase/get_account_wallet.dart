cd0b75355c089855c8cc761291c892b7fdd22d8d
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-12 15:01:14 +0700

    feat: update execute method in GetAccountWalletUseCase to accept AuthUserEntities for user-specific wallet retrieval

--- lib/features/expense/domain/usecase/get_account_wallet.dart
+++ lib/features/expense/domain/usecase/get_account_wallet.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/features/auth/domain/entity/auth_user_entities.dart';
 import 'package:expense_tracker/features/expense/data/model/account_wallet_model.dart';
 import 'package:expense_tracker/features/expense/data/repository/transaction_repository_impl.dart';
 
@@ -5,7 +6,8 @@ class GetAccountWalletUseCase {
   final TransactionRepositoryImpl _transactionRepositoryImpl;
   GetAccountWalletUseCase(this._transactionRepositoryImpl);
 
-  Future<List<AccountWalletModel>> execute() {
-    return _transactionRepositoryImpl.getAccountWallet();
+  Future<List<AccountWalletModel>> execute(
+      AuthUserEntities authUser) {
+    return _transactionRepositoryImpl.getAccountWallet(authUser);
   }
 }
