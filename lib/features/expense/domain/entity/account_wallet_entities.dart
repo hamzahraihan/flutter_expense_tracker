f5b7aa2986fc7651e9d53dcad43d7f918453e434
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-12 15:34:28 +0700

    feat: refactor AccountWalletEntities to include walletName and change balance type to int

--- lib/features/expense/domain/entity/account_wallet_entities.dart
+++ lib/features/expense/domain/entity/account_wallet_entities.dart
@@ -1,26 +1,15 @@
 import 'package:equatable/equatable.dart';
 
-enum WalletType { paypal, jago, bca, mandiri }
-
-extension WalletTypeExtension on WalletType {
-  String toShortString() {
-    return toString().split('.').last;
-  }
-
-  static WalletType fromString(String value) {
-    return WalletType.values
-        .firstWhere((e) => e.toShortString() == value);
-  }
-}
-
 class AccountWalletEntities extends Equatable {
   final String uid;
-  final String balance;
-  final WalletType walletType;
+  final int balance;
+  final String walletName;
+  final String walletType;
 
   const AccountWalletEntities({
     required this.uid,
     required this.balance,
+    required this.walletName,
     required this.walletType,
   });
 
@@ -28,6 +17,7 @@ class AccountWalletEntities extends Equatable {
   List<Object?> get props => [
         uid,
         balance,
+        walletName,
         walletType,
       ];
 }
