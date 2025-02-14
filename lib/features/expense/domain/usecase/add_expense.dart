9330db3e40c30d611c5ad5ca38cd5d71cfd54735
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-14 14:19:16 +0700

    feat: update AddExpenseUseCase to include walletId and authUser for enhanced transaction processing

--- lib/features/expense/domain/usecase/add_expense.dart
+++ lib/features/expense/domain/usecase/add_expense.dart
@@ -1,11 +1,13 @@
+import 'package:expense_tracker/features/auth/domain/entity/auth_user_entities.dart';
 import 'package:expense_tracker/features/expense/data/repository/transaction_repository_impl.dart';
 
 class AddExpenseUseCase {
   final TransactionRepositoryImpl _transactionRepositoryImpl;
   AddExpenseUseCase(this._transactionRepositoryImpl);
 
-  Future<void> execute(Map<String, dynamic> transaction) async {
-    return await _transactionRepositoryImpl
-        .addExpenseTransaction(transaction);
+  Future<void> execute(String walletId, AuthUserEntities authUser,
+      Map<String, dynamic> transaction) async {
+    return await _transactionRepositoryImpl.addExpenseTransaction(
+        walletId, authUser, transaction);
   }
 }
