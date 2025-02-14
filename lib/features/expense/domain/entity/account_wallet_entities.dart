305bedd5c4130d42370902225a5aa6d1d2fad875
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2025-02-14 14:19:04 +0700

    feat: add id field to AccountWalletEntities for improved entity identification

--- lib/features/expense/domain/entity/account_wallet_entities.dart
+++ lib/features/expense/domain/entity/account_wallet_entities.dart
@@ -1,12 +1,14 @@
 import 'package:equatable/equatable.dart';
 
 class AccountWalletEntities extends Equatable {
+  final String id;
   final String uid;
   final int balance;
   final String walletName;
   final String walletType;
 
   const AccountWalletEntities({
+    required this.id,
     required this.uid,
     required this.balance,
     required this.walletName,
@@ -15,6 +17,7 @@ class AccountWalletEntities extends Equatable {
 
   @override
   List<Object?> get props => [
+        id,
         uid,
         balance,
         walletName,
